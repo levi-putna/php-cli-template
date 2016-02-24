9ce732aad2c00b22c345f2ab1ea03bd8b0f63233
Levi Putna <levi.putna@gmail.com>
2016-02-24 20:20:38 +1000

    Add db test

--- tests/ResponseTest.php
+++ tests/ResponseTest.php
@@ -1,11 +1,11 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: leviputna
  * Date: 16/12/2015
  * Time: 5:28 PM
  */
-
 class CitiesResponseTest extends PHPUnit_Framework_TestCase
 {
 
@@ -21,5 +21,20 @@ class CitiesResponseTest extends PHPUnit_Framework_TestCase
         $info = $response = file_get_contents($uri);
 
         $this->assertContains("Amsterdam", $info);
+    }
+
+    public function testDatabase()
+    {
+        $hostname = 'localhost';
+        $username = 'root';
+
+        try {
+            $dbh = new PDO("mysql:host=$hostname;dbname=sample", $username);
+            $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->assertFalse(false);
+
+        } catch (PDOException $e) {
+            $this->assertFalse(true, $e->getMessage());
+        }
     }
 }
